c53beb67cdf2a2ea8ec9f901bd75d5cd6cd9e963
sivarangasrinivasan <148032132+sivarangasrinivasan@users.noreply.github.com>
2024-08-31 00:26:47 +0530

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>Copyright &copy; 2024 Brought To You By<a href="https://github.com/sivarangasrinivasan">siva ranga srinivasan</a></strong>
+</footer>
